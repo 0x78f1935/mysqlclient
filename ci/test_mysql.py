edec79aede4fd52d2458b379b087ba8f63b4c585
0x78f1935 <annihilator708@outlook.com>
2025-10-28 19:11:53 +0100

    FIX: Character encoding

--- ci/test_mysql.py
+++ ci/test_mysql.py
@@ -19,7 +19,7 @@ DATABASES = {
         "HOST": "127.0.0.1",
         "USER": "scott",
         "PASSWORD": "tiger",
-        "TEST": {"CHARSET": "utf8mb3", "COLLATION": "utf8mb3_general_ci"},
+        "TEST": {"CHARSET": "utf8mb4", "COLLATION": "utf8mb4_general_ci"},
     },
     "other": {
         "ENGINE": "django.db.backends.mysql",
@@ -27,7 +27,7 @@ DATABASES = {
         "HOST": "127.0.0.1",
         "USER": "scott",
         "PASSWORD": "tiger",
-        "TEST": {"CHARSET": "utf8mb3", "COLLATION": "utf8mb3_general_ci"},
+        "TEST": {"CHARSET": "utf8mb4", "COLLATION": "utf8mb4_general_ci"},
     },
 }
 
